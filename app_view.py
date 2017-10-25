d54acf9b671a1046407a19abae5a62310f0bde9c
Wu Yuanhao <wuyuanhao@anji-logistics.com>
2017-10-25 12:36:18 +0800

    pickle

--- app_view.py
+++ app_view.py
@@ -3,6 +3,7 @@ from flask import jsonify, request
 from app import celery
 from dataProcess.processing import GAVARP_Process_json
 from algorithmModel.algorithm_entry import optimization
+import json
 
 @flask.route('/')
 def index():
@@ -12,6 +13,8 @@ def index():
 @flask.route('/start',methods=['POST'])
 def start_task():
     data = request.get_json()
+    with open('input_data.json', 'w') as f:
+        json.dump(data,f)
     input_data = GAVARP_Process_json(data)
     if not input_data == -1:
         print 'parsing successfully'
