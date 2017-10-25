f1719dab491fbbca961cd09daae1ae79f7d11aab
qinfen <chaoqun.yue@newtouch.cn>
2017-10-25 13:34:31 +0800

    add

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ import logging
 flask = Flask(__name__)
 flask.config.from_object(AppConfig)
 
-# celery=Celery(CeleryConfig.MAIN_NAME, broker=CeleryConfig.BROKER_ADDRESS,
-#               task_serializer=CeleryConfig.CELERY_TASK_SERIALIZER)
-celery = Celery("app",broker="amqp://localhost//")
+celery=Celery(CeleryConfig.MAIN_NAME, broker=CeleryConfig.BROKER_ADDRESS,
+              task_serializer=CeleryConfig.CELERY_TASK_SERIALIZER)
+
 
 
 logger=Logger(config=LoggerConfig)
--- app_view.py
+++ app_view.py
@@ -3,11 +3,17 @@ from flask import jsonify, request
 from app import celery
 from dataProcess.processing import GAVARP_Process_json
 from algorithmModel.algorithm_entry import optimization
+import time
+import os
 
 @flask.route('/')
 def index():
     return "hello world!"
 
+@flask.route("/revoke")
+def test_revoke():
+    res = add.delay(3,2)
+    return "test revoke over"
 
 @flask.route('/start',methods=['POST'])
 def start_task():
@@ -26,6 +32,11 @@ def connect_Test():
     return 'connection ok'
 
 
-@celery.task
-def add(x,y):
+@celery.task(bind=True,track_started=True)
+def add(self,x,y):
+    # self.update_state(meta={"taskpid":os.getpid()})
+    time.sleep(30)
     return x+y
+
+def on_raw_message(body):
+    print body
